410ffedd1bef7091c555a49fafbef2c41fe356f4
Harry P <habs@sdf.org>
2021-04-08 14:05:41 -0400

    L4 done

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks',
-            image: './assets/images/socks_green.jpg',
-            description: 'Footwear',
-            url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-        }
+  data() {
+    return {
+      product: 'Socks',
+      image: './assets/images/socks_green.jpg',
+      description: 'Footwear',
+      url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
+      inventory: 100,
+      onSale: true,
     }
+  }
 })
