9b8885adb34880e93e82487a8317797c0780c221
Harry P <habs@sdf.org>
2021-04-08 14:57:54 -0400

    finish L8

--- main.js
+++ main.js
@@ -2,32 +2,51 @@ const app = Vue.createApp({
   data() {
     return {
       cart: 0,
-      inStock: false,
       product: 'Socks',
-      image: './assets/images/socks_green.jpg',
+      brand: 'Vue Mastery',
       description: 'Footwear',
       url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
-      inventory: 20,
       onSale: true,
       details: ['50% cotton', '30% wool', '20% polyester'],
       variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 }
       ],
+      selectedVariant: 0,
       sizes: [ 'small', 'medium', 'large' ],
     }
   },
   methods: {
     addToCart() {
       this.cart++;
-      this.inventory--;
+      this.variants[this.selectedVariant].quantity--;
     },
     removeFromCart() {
       this.cart--;
-      this.inventory++;
+      this.variants[this.selectedVariant].quantity++;
     },
     updateImage(variantImage) {
       this.image = variantImage;
+    },
+    updateVariant(index) {
+      this.selectedVariant = index;
+    }
+  },
+  computed: {
+    title() {
+      return this.brand + ' ' + this.product;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity > 0;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inventory() {
+      return this.variants[this.selectedVariant].quantity;
+    },
+    onSaleMsg() {
+      return `${this.brand} ${this.product} is on sale`;
     }
   }
 })
