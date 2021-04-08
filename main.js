7465d6c30bb1a3155202d9b74c24003b848214d5
Harry P <habs@sdf.org>
2021-04-08 14:11:34 -0400

    finish l5

--- main.js
+++ main.js
@@ -7,6 +7,12 @@ const app = Vue.createApp({
       url: 'https://www.vuemastery.com/courses/intro-to-vue-3/attribute-binding-vue3',
       inventory: 100,
       onSale: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: [ 'small', 'medium', 'large' ]
     }
   }
 })
